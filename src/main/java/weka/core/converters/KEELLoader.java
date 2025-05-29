edbb5a7ca557c4bb6581dd6b6bd437735192b8ab
Txus Pérez <txus.perez@ehu.eus>
2025-05-29 12:06:14 +0200

    Goiburukoaren iruzkina gehituta.

--- src/main/java/weka/core/converters/KEELLoader.java
+++ src/main/java/weka/core/converters/KEELLoader.java
@@ -1,5 +1,24 @@
-/**
- * 
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *    KEELLoader.java
+ *    Copyright (C) 2024 ALDAPA Team (http://www.aldapa.eus)
+ *    Faculty of Informatics, Donostia, 20018
+ *    University of the Basque Country (UPV/EHU), Basque Country
+ *    
  */
 package weka.core.converters;
 
@@ -28,7 +47,7 @@ import weka.core.Utils;
  * <!-- globalinfo-end -->
  * 
  * @author Jesús M. Pérez (txus.perez@ehu.eus)
- * @version $Revision: 0 $
+ * @version $Revision: 1.0 $
  * @see Loader
 */
 public class KEELLoader extends AbstractFileLoader  implements BatchConverter {
