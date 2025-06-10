9444df0e5d650bb0ecb42534e3e7a4fc251e3722
Txus Pérez <txus.perez@ehu.eus>
2025-06-10 17:08:39 +0200

    Level by level araberako klasea sortuta.
    buildClassifier() eta buildPartialTreeItera() espezifikoak.
    buildClassifier()-etik setNumberNodesToBeConsolidated()-ra kodea aterata
    adabegi kopurua ezartzeko, barneko adabegi kopuruaren edo zuhaitzaren
    maila kopuruaren arabera.

---
+++ src/main/java/weka/classifiers/trees/j48PartiallyConsolidated/C45ItLevelPartiallyConsolidatedPruneableClassifierTree.java
@@ -0,0 +1,276 @@
+package weka.classifiers.trees.j48PartiallyConsolidated;
+
+import java.util.ArrayList;
+
+import weka.classifiers.trees.J48PartiallyConsolidated;
+import weka.classifiers.trees.j48.ModelSelection;
+import weka.core.Instances;
+import weka.core.Utils;
+
+/**
+ * Class for handling a 'partial' consolidated tree structure,
+ * based on a consolidation percentage or on a specific value,
+ * that indicate the number of levels of the tree to be developed. 
+ * The tree is processed iteratively (instead of recursively like 
+ * the original version).
+ * (m_priorityCriteria == J48PartiallyConsolidated.PriorCrit_Levelbylevel)
+ * 
+ * @author Jesús M. Pérez (txus.perez@ehu.eus)
+ * @author Josué Cabezas Regoyo
+ * @version $Revision: 1.0 $
+ */
+public class C45ItLevelPartiallyConsolidatedPruneableClassifierTree
+		extends C45ItPartiallyConsolidatedPruneableClassifierTree {
+
+	private static final long serialVersionUID = 7265782633965535703L;
+
+	public C45ItLevelPartiallyConsolidatedPruneableClassifierTree(ModelSelection toSelectLocModel,
+			C45ModelSelectionExtended baseModelToForceDecision, boolean pruneTree, float cf, boolean raiseTree,
+			boolean cleanup, boolean collapseTree, int numberSamples, int numberConsoNodesHowToSet,
+			int priorityCriteria, int heuristicSearchAlgorithm, boolean pruneCT, boolean collapseCT,
+			boolean notPreservingStructure) throws Exception {
+		super(toSelectLocModel, baseModelToForceDecision, pruneTree, cf, raiseTree, cleanup, collapseTree, numberSamples,
+				numberConsoNodesHowToSet, priorityCriteria, heuristicSearchAlgorithm, pruneCT, collapseCT,
+				notPreservingStructure);
+	}
+
+	/**
+	 * Method for building a pruneable classifier partial consolidated tree;
+	 * but based on a number of levels of the tree to be developed (indicated 
+	 * as a percentage of the number of levels of the whole consolidated tree
+	 * or a specific value). 
+	 *
+	 * @param data                 the data for pruning the consolidated tree
+	 * @param samplesVector        the vector of samples for building the
+	 *                             consolidated tree
+	 * @param consolidationPercent the value of consolidation percent
+	 * @throws Exception if something goes wrong
+	 */
+	public void buildClassifier(Instances data, Instances[] samplesVector, float consolidationPercent) throws Exception {
+		long trainTimeStart = 0, trainTimeElapsed = 0;
+
+		setNumberNodesToBeConsolidated(data, samplesVector, consolidationPercent);
+		// buildTree
+		trainTimeStart = System.currentTimeMillis();
+		buildPartialTreeItera(data, samplesVector, m_subtreeRaising || !m_cleanup);
+		if (m_collapseTheCTree) {
+			collapse();
+		}
+		if (m_pruneTheConsolidatedTree) {
+			prune();
+		}
+		trainTimeElapsed = System.currentTimeMillis() - trainTimeStart;
+		System.out.println("Time taken to build the partial consolidated tree: " + Utils.doubleToString(trainTimeElapsed / 1000.0, 2) + " seconds\n");
+		m_elapsedTimeTrainingPartialCT = trainTimeElapsed / (double)1000.0;
+
+		trainTimeStart = System.currentTimeMillis();
+		applyBagging();
+		trainTimeElapsed = System.currentTimeMillis() - trainTimeStart;
+		System.out.println("Time taken to build the associated Bagging: " + Utils.doubleToString(trainTimeElapsed / 1000.0, 2) + " seconds\n");
+		m_elapsedTimeTrainingAssocBagging = trainTimeElapsed / (double)1000.0;
+
+		if (m_cleanup)
+			cleanup(new Instances(data, 0));
+		if(!m_isLeaf)
+			computeNumberBaseTreesPreservingPartialCTStructure();
+	}
+
+	/**
+	 * Determines the number of levels of the partial tree to be developed based 
+	 * on a percentage value with respect to the number of levels of the whole 
+	 * consolidated tree (which must first be constructed) or based on a specific 
+	 * value (passed as a parameter).
+	 * 
+	 * @param data                 the data for pruning the consolidated tree
+	 * @param samplesVector        the vector of samples for building the
+	 *                             consolidated tree
+	 * @param consolidationPercent the value of consolidation percent
+	 * @throws Exception if something goes wrong
+	 */
+	public void setNumberNodesToBeConsolidated(Instances data, Instances[] samplesVector, float consolidationPercent) throws Exception {
+		if (m_numberConsoNodesHowToSet == J48PartiallyConsolidated.NumberConsoNodes_Percentage) {
+			long trainTimeStart = 0, trainTimeElapsed = 0;
+			
+			trainTimeStart = System.currentTimeMillis();
+			super.buildTree(data, samplesVector, m_subtreeRaising || !m_cleanup); // build the tree without restrictions
+							
+			if (m_collapseTheCTree) {
+				collapse();
+			}
+			if (m_pruneTheConsolidatedTree) {
+				prune();
+			}
+			trainTimeElapsed = System.currentTimeMillis() - trainTimeStart;
+			System.out.println("Time taken to build the whole consolidated tree: " + Utils.doubleToString(trainTimeElapsed / 1000.0, 2) + " seconds\n");
+			m_elapsedTimeTrainingWholeCT = trainTimeElapsed / (double)1000.0;
+
+			// m_priorityCriteria == J48PartiallyConsolidated.PriorCrit_Levelbylevel
+
+			// Number of levels of the consolidated tree
+			int treeLevels = numLevels();
+
+			// Number of levels of the consolidated tree to leave as consolidated based on
+			// given consolidationPercent
+			int numberLevelsConso = (int) (((treeLevels * consolidationPercent) / 100) + 0.5);
+			m_maximumCriteria = numberLevelsConso;
+			setNumInternalNodesConso(numberLevelsConso);
+			System.out.println(
+					"Number of levels to leave as consolidated: " + numberLevelsConso + " of " + treeLevels);
+
+		} else // consolidationNumberHowToSet ==
+				// J48PartiallyConsolidated.ConsolidationNumber_Value
+		{
+			m_maximumCriteria = (int) consolidationPercent;
+			System.out.println("Number of levels to leave as consolidated: " + m_maximumCriteria);
+			m_elapsedTimeTrainingWholeCT = (double)0.0;
+		}
+	}
+	
+	/**
+	 * Builds the partial consolidated tree structure, in this case
+	 * iteratively (instead of recursively as in the original method, buildTree()).
+	 *
+	 * @param data          the data for pruning the consolidated tree
+	 * @param samplesVector the vector of samples used for consolidation
+	 * @param keepData      is training data to be kept?
+	 * @throws Exception if something goes wrong
+	 */
+	public void buildPartialTreeItera(Instances data, Instances[] samplesVector, boolean keepData) throws Exception {
+		/** Number of Samples. */
+		int numberSamples = samplesVector.length;
+
+		/** Initialize the consolidated tree */
+		initiliazeTree(data, keepData);
+		/** Initialize the base trees */
+		for (int iSample = 0; iSample < numberSamples; iSample++)
+			m_sampleTreeVector[iSample].initiliazeTree(samplesVector[iSample], keepData);
+
+		ArrayList<Object[]> list = new ArrayList<>();
+
+		// add(Data, samplesVector, tree, orderValue, currentLevel)
+		list.add(new Object[] { data, samplesVector, this, null, 0 }); // The parent node is considered level 0
+
+		int index = 0;
+
+		while (list.size() > 0) {
+
+			Object[] current = list.get(0);
+
+			/** Current node level. **/
+			int currentLevel = (int) current[4];
+
+			/** Number of Samples. */
+			Instances[] currentSamplesVector = (Instances[]) current[1];
+
+			list.set(0, null); // Null to free up memory
+			list.remove(0);
+
+			Instances currentData = (Instances) current[0];
+			C45ItPartiallyConsolidatedPruneableClassifierTree currentTree = (C45ItLevelPartiallyConsolidatedPruneableClassifierTree) current[2];
+			currentTree.m_order = index;
+
+			/** Initialize the consolidated tree */
+			currentTree.initiliazeTree(currentData, keepData);
+			/** Initialize the base trees */
+			for (int iSample = 0; iSample < numberSamples; iSample++)
+				currentTree.m_sampleTreeVector[iSample].initiliazeTree(currentSamplesVector[iSample], keepData);
+
+			/**
+			 * Select the best model to split (if it is worth) based on the consolidation
+			 * proccess
+			 */
+			currentTree.setLocalModel(currentData, currentSamplesVector);
+			for (int iSample = 0; iSample < numberSamples; iSample++)
+				currentTree.m_sampleTreeVector[iSample].setLocalModel(currentSamplesVector[iSample],
+						currentTree.getLocalModel());
+
+			if ((currentTree.getLocalModel().numSubsets() > 1) && (currentLevel < m_maximumCriteria)) {
+
+				/** Vector storing the obtained subsamples after the split of data */
+				Instances[] localInstances;
+				/**
+				 * Vector storing the obtained subsamples after the split of each sample of the
+				 * vector
+				 */
+				ArrayList<Instances[]> localInstancesVector = new ArrayList<Instances[]>();
+
+				/**
+				 * For some base trees, although the current node is not a leaf, it could be
+				 * empty. This is necessary in order to calculate correctly the class membership
+				 * probabilities for the given test instance in each base tree
+				 */
+
+				ArrayList<Object[]> listSons = new ArrayList<>();
+
+				for (int iSample = 0; iSample < numberSamples; iSample++)
+					if (Utils.eq(currentTree.m_sampleTreeVector[iSample].getLocalModel().distribution().total(), 0))
+						currentTree.m_sampleTreeVector[iSample].setIsEmpty(true);
+
+				/** Split data according to the consolidated m_localModel */
+				localInstances = currentTree.getLocalModel().split(currentData);
+				for (int iSample = 0; iSample < numberSamples; iSample++)
+					localInstancesVector.add(currentTree.getLocalModel().split(currentSamplesVector[iSample]));
+
+				/**
+				 * Create the child nodes of the current node and call recursively to
+				 * getNewTree()
+				 */
+				currentData = null;
+				currentSamplesVector = null;
+				currentTree.createSonsVector(currentTree.getLocalModel().numSubsets());
+				for (int iSample = 0; iSample < numberSamples; iSample++)
+					((C45PruneableClassifierTreeExtended) currentTree.m_sampleTreeVector[iSample])
+							.createSonsVector(currentTree.getLocalModel().numSubsets());
+
+				//////////////////
+				C45ModelSelectionExtended baseModelToForceDecision = 
+						currentTree.m_sampleTreeVector[0].getBaseModelToForceDecision();
+				for (int iSon = 0; iSon < currentTree.getSons().length; iSon++) {
+					/** Vector storing the subsamples related to the iSon-th son */
+					Instances[] localSamplesVector = new Instances[numberSamples];
+					for (int iSample = 0; iSample < numberSamples; iSample++)
+						localSamplesVector[iSample] = ((Instances[]) localInstancesVector.get(iSample))[iSon];
+
+					// getNewTree
+					C45ItPartiallyConsolidatedPruneableClassifierTree newTree = new C45ItLevelPartiallyConsolidatedPruneableClassifierTree(
+							currentTree.getToSelectModel(), baseModelToForceDecision, m_pruneTheTree, m_CF,
+							m_subtreeRaising, m_cleanup, m_collapseTheTree, localSamplesVector.length,
+							m_numberConsoNodesHowToSet,
+							m_priorityCriteria, m_heuristicSearchAlgorithm, m_pruneTheConsolidatedTree, m_collapseTheCTree,
+							m_pruneBaseTreesWithoutPreservingConsolidatedStructure);
+
+					/** Set the recent created base trees like the sons of the given parent node */
+					for (int iSample = 0; iSample < numberSamples; iSample++)
+						((C45PruneableClassifierTreeExtended) currentTree.m_sampleTreeVector[iSample]).setIthSon(iSon,
+								newTree.m_sampleTreeVector[iSample]);
+
+					listSons.add(new Object[] { localInstances[iSon], localSamplesVector, newTree, 0,
+							currentLevel + 1 });
+
+					currentTree.setIthSon(iSon, newTree);
+
+					localInstances[iSon] = null;
+					localSamplesVector = null;
+				}
+				list.addAll(listSons);
+
+				localInstances = null;
+				localInstancesVector.clear();
+				listSons = null;
+			} else {
+				currentTree.setIsLeaf(true);
+				for (int iSample = 0; iSample < numberSamples; iSample++)
+					currentTree.m_sampleTreeVector[iSample].setIsLeaf(true);
+
+				if (Utils.eq(currentTree.getLocalModel().distribution().total(), 0)) {
+					currentTree.setIsEmpty(true);
+					for (int iSample = 0; iSample < numberSamples; iSample++)
+						currentTree.m_sampleTreeVector[iSample].setIsEmpty(true);
+				}
+				currentData = null;
+				currentSamplesVector = null;
+			}
+			index++;
+		}
+	}
+}
